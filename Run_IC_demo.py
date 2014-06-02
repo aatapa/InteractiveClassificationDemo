eecc886187b627696ac774dd70ffd7a512c47eda
Tapio Pahikkala <tapio.pahikkala@utu.fi>
2014-06-02 23:39:15 +0300

    Cleaned up the code and added some instructions.

--- Run_IC_demo.py
+++ Run_IC_demo.py
@@ -23,8 +23,13 @@ from rlscore.learner.interactive_rls_classifier import InteractiveRlsClassifier
 #img_orig=mpimg.imread('8068.jpg')
 #img_orig=mpimg.imread('299091.jpg')
 
+#img_orig=mpimg.imread('IMG_7459_osakuva.png')
+#Xmat = np.loadtxt('features_IMG_7459_osakuva.txt')
+
+
 img_orig=mpimg.imread('198023.jpg')
 Xmat = np.loadtxt('features_198023.txt')
+
 Xmat = np.nan_to_num(Xmat)
 
 #img_orig[:,:,1]=0
@@ -32,11 +37,6 @@ Xmat = np.nan_to_num(Xmat)
 #featgen.create_features(img_orig[:,:,0], 9, 1, 1, 8, 1, 1)
 #foo
 img=img_orig.copy()
-#print img.shape, img.dtype
-#print Xmat
-#Xmat = Xmat[:, 2:]
-#print Xmat
-#print Xmat.shape
 
 pointrange = np.arange(img.shape[0]*img.shape[1])
 rows, cols = np.unravel_index(pointrange, (img.shape[0], img.shape[1]))
@@ -47,37 +47,40 @@ pcoll = np.vstack([cols, rows]).T
 #Xmat = np.hstack([Xmat, pcoll])
 classcount = 2
 
-rpool = {}
+kwargs = {}
 bias = 0.
 
-rpool['train_features'] = Xmat
-rpool['train_labels'] = np.zeros((Xmat.shape[0]), dtype = np.int32)
-rpool['kernel'] = 'GaussianKernel'
-rpool['bias'] = bias
-rpool['gamma'] = 2. ** (-17.)
-rpool['regparam'] = 2. ** (1.)
-rpool['number_of_clusters'] = classcount
-rpool['basis_vectors'] = pyrandom.sample(range(Xmat.shape[0]), 100)
-#print rpool['basis_vectors']
+kwargs['train_features'] = Xmat
+kwargs['train_labels'] = np.zeros((Xmat.shape[0]), dtype = np.int32)
+kwargs['kernel'] = 'GaussianKernel'
+kwargs['bias'] = bias
+kwargs['gamma'] = 2. ** (-17.)
+kwargs['regparam'] = 2. ** (1.)
+kwargs['number_of_clusters'] = classcount
+kwargs['basis_vectors'] = pyrandom.sample(range(Xmat.shape[0]), 100)
+#print kwargs['basis_vectors']
 
-mmc = InteractiveRlsClassifier.createLearner(**rpool)
+mmc = InteractiveRlsClassifier.createLearner(**kwargs)
 
 plt.ion()
-#plt.plot([1,2],[3,4],'r^')
 
 mmc.train()
-#print mmc.resource_pool
 
 mmc.working_set = None
 mmc.wsc = None
 
-#axcolor = 'lightgoldenrodyellow'
 
+slider_coords = [0.25, 0.06, 0.65, 0.02]
+obj_fun_display_coords = [0.25, 0.96, 0.65, 0.02]
 
 class SelectFromCollection(object):
     
     def __init__(self, ax, collection, mmc, img):
         self.imdata = plt.gca().imshow(img)
+        plt.setp(ax.get_yticklabels(), visible=False)
+        ax.yaxis.set_tick_params(size=0)
+        plt.setp(ax.get_xticklabels(), visible=False)
+        ax.xaxis.set_tick_params(size=0)
         self.img = img
         self.canvas = ax.figure.canvas
         self.collection = collection
@@ -88,7 +91,6 @@ class SelectFromCollection(object):
         self.xys = collection
         self.Npts = len(self.xys)
         
-        self.neighset = set([])
         self.lockedset = set([])
 
         self.lasso = LassoSelector(ax, onselect=self.onselect)#, lineprops = {:'prism'})
@@ -100,52 +102,12 @@ class SelectFromCollection(object):
         self.lasso.connect_event('key_press_event', self.onkeypressed)
         #self.lasso.connect_event('button_release_event', self.onrelease)
         self.ind = []
-        self.slider_axis = plt.axes([0.25, 0.1, 0.65, 0.03], visible = False)
-        self.slider_axis2 = plt.axes([0.25, 0.07, 0.65, 0.03], visible = False)
-        #self.in_selection_slider = Slider(self.slider_axis, 'Fraction', 0., 30.0, valinit=f0)
-        #    #fig.canvas.draw_idle()
-        #self.in_selection_slider.on_changed(sliderupdate)
-        #self.slider_axis = None
+        self.slider_axis = plt.axes(slider_coords, visible = False)
+        self.slider_axis2 = plt.axes(obj_fun_display_coords, visible = False)
         self.in_selection_slider = None
-    
-    def newSlider(self):
-        #print 'newslider'
-        nozeros = np.nonzero(self.mmc.classvec_ws)[0]
-        self.slider_axis.cla()
-        del self.slider_axis
-        del self.slider_axis2
-        self.slider_axis = plt.axes([0.25, 0.1, 0.65, 0.03])
-        self.slider_axis2 = plt.axes([0.25, 0.07, 0.65, 0.03])
-        steepness_vector = mmc.compute_steepness_vector()
-        X = [steepness_vector, steepness_vector]
-        #right = left+width
-        #self.slider_axis2.imshow(X, interpolation='bicubic', cmap=plt.get_cmap("Blues"), alpha=1)
-        self.slider_axis2.imshow(X, cmap=plt.get_cmap("Blues"))
-        self.slider_axis2.set_aspect('auto')
-        del self.in_selection_slider
-        self.in_selection_slider = None
-        self.in_selection_slider = Slider(self.slider_axis, 'Fraction', 0., len(mmc.working_set), valinit=len(nozeros))
-        def sliderupdate(val):
-            val = int(val)
-            nonzeroc = len(np.nonzero(self.mmc.classvec_ws)[0])
-            if val > nonzeroc:
-                claims = val - nonzeroc
-                newclazz = 1
-            elif val < nonzeroc:
-                claims = nonzeroc - val
-                newclazz = 0
-            else: return
-            print val, nonzeroc, claims
-            self.claims = claims
-            mmc.claim_n_points(claims, newclazz)
-            steepness_vector = mmc.compute_steepness_vector()
-            X = [steepness_vector, steepness_vector]
-            self.slider_axis2.imshow(X, cmap=plt.get_cmap("Blues"))
-            self.slider_axis2.set_aspect('auto')
-            self.recolor()
-            self.prevnewclazz = newclazz
-        self.in_selection_slider.on_changed(sliderupdate)
-        self.redrawline()
+        newws = list(set(range(len(self.collection))) - self.lockedset)
+        self.mmc.new_working_set(newws)
+        self.lasso.line.set_visible(False)
     
     def onselect(self, verts):
         self.path = Path(verts)
@@ -153,7 +115,7 @@ class SelectFromCollection(object):
         print 'Selected '+str(len(self.ind))+' points'
         newws = list(set(self.ind) - self.lockedset)
         self.mmc.new_working_set(newws)
-        self.newSlider()
+        self.redrawall()
     
     def onpress(self, event):
         if self.lasso.ignore(event) or event.inaxes != self.ax:
@@ -172,67 +134,75 @@ class SelectFromCollection(object):
         self.lasso.verts = None
     
     def onkeypressed(self, event):
-        print('you pressed', event.key) #, event.xdata, event.ydata
-        if event.key.isdigit() and mmc.working_set != None:
-            newclazz = int(event.key)
-            if newclazz >= 0 and newclazz < classcount:
-                
-                #self.mmc.new_working_set(list(self.neighset))
-                
-                steepestdir = mmc.claim_a_point(newclazz)
-                col, row = self.collection[steepestdir]
-                print steepestdir, row, col
-                self.recolor()
-                
-                '''
-                self.neighset.discard(coords_to_ind[row, col])
-                for i in range(-1, 2):
-                    for j in range(-1, 2):
-                        nind = coords_to_ind[row + i, col + j]
-                        if mmc.classvec[nind] == 0: self.neighset.add(nind)
-                print self.neighset
-                '''
-                
-                self.prevnewclazz = newclazz
-        if event.key == 'a': # and self.prevnewclazz != None:
+        print 'You pressed', event.key
+        if event.key == '1':
+            print 'Assigned all selected points to class 1'
             newclazz = 1
             mmc.claim_all_points_in_working_set(newclazz)
-            #col_row = self.collection[mmc.working_set]
-            self.recolor()
-            #nonzeros_ws = np.nonzero(self.mmc.classvec_ws)[0]
-            '''
-            self.neighset -= set(self.mmc.classvec_ws)
-            img.shape[0], img.shape[1]
-            for pind in range(col_row.shape[0]):
-                colind, rowind = col_row[pind, 1], col_row[pind, 0]
-                #if rowind > 0:
-                #    if colind > 0:
-                for i in range(-1, 2):
-                    for j in range(-1, 2):
-                        nind = coords_to_ind[colind + j, rowind + i]
-                        if mmc.classvec[nind] == 0: self.neighset.add(nind)
-            print self.neighset'''
-        if event.key == 'n': # and self.prevnewclazz != None:
+        if event.key == '0':
+            print 'Assigned all selected points to class 0'
             newclazz = 0
             mmc.claim_all_points_in_working_set(newclazz)
-            #col_row = self.collection[mmc.working_set]
-            self.recolor()
+        if event.key == 'a':
+            print 'Selected all points'
+            newws = list(set(range(len(self.collection))) - self.lockedset)
+            self.mmc.new_working_set(newws)
+            self.lasso.line.set_visible(False)
         if event.key == 'c':
             changecount = mmc.cyclic_descent_in_working_set()
-            print changecount
-            self.recolor()
+            print 'Performed ', changecount, 'cyclic descent steps'
         if event.key == 'l':
+            print 'Locked the class labels of selected points'
             self.lockedset = self.lockedset | set(self.ind)
             newws = list(set(self.ind) - self.lockedset)
             self.mmc.new_working_set(newws)
-            print newws
+            #print newws
         if event.key == 'u':
+            print 'Unlocked the selected points'
             self.lockedset = self.lockedset - set(self.ind)
             newws = list(set(self.ind) - self.lockedset)
             self.mmc.new_working_set(newws)
-        self.newSlider()
+        self.redrawall()
     
-    def recolor(self):
+    def redrawall(self, newslider = True):
+        if newslider:
+            nozeros = np.nonzero(self.mmc.classvec_ws)[0]
+            self.slider_axis.cla()
+            del self.slider_axis
+            del self.slider_axis2
+            self.slider_axis = plt.axes(slider_coords)
+            self.slider_axis2 = plt.axes(obj_fun_display_coords)
+            steepness_vector = mmc.compute_steepness_vector()
+            X = [steepness_vector, steepness_vector]
+            #right = left+width
+            #self.slider_axis2.imshow(X, interpolation='bicubic', cmap=plt.get_cmap("Oranges"), alpha=1)
+            self.slider_axis2.imshow(X, cmap=plt.get_cmap("Oranges"))
+            self.slider_axis2.set_aspect('auto')
+            plt.setp(self.slider_axis2.get_yticklabels(), visible=False)
+            self.slider_axis2.yaxis.set_tick_params(size=0)
+            del self.in_selection_slider
+            self.in_selection_slider = None
+            self.in_selection_slider = Slider(self.slider_axis, 'Fraction slider', 0., len(mmc.working_set), valinit=len(nozeros))
+            def sliderupdate(val):
+                val = int(val)
+                nonzeroc = len(np.nonzero(self.mmc.classvec_ws)[0])
+                if val > nonzeroc:
+                    claims = val - nonzeroc
+                    newclazz = 1
+                elif val < nonzeroc:
+                    claims = nonzeroc - val
+                    newclazz = 0
+                else: return
+                print 'Claimed', claims, 'points for class', newclazz   #val, nonzeroc, claims
+                self.claims = claims
+                mmc.claim_n_points(claims, newclazz)
+                steepness_vector = mmc.compute_steepness_vector()
+                X = [steepness_vector, steepness_vector]
+                self.slider_axis2.imshow(X, cmap=plt.get_cmap("Oranges"))
+                self.slider_axis2.set_aspect('auto')
+                self.redrawall(newslider = False) #HACK!
+                self.prevnewclazz = newclazz
+            self.in_selection_slider.on_changed(sliderupdate)
         oneclazz = np.nonzero(self.mmc.classvec)[0]
         col_row = self.collection[oneclazz]
         rowcs, colcs = col_row[:, 1], col_row[:, 0]
@@ -244,24 +214,8 @@ class SelectFromCollection(object):
         col_row = self.collection[zeroclazz]
         rowcs, colcs = col_row[:, 1], col_row[:, 0]
         self.img[rowcs, colcs, :] = img_orig[rowcs, colcs, :]
-        '''
-        if newclazz == 1:
-            #self.img[rowcs, colcs, :] = 0
-            #print self.img[rowcs, colcs, 0]
-            #self.img[rowcs, colcs, 0] = np.fmin(255, self.img[rowcs, colcs, 0]+50)
-            self.img[rowcs, colcs, :] += 128
-            #print self.img[rowcs, colcs, 0]
-        else:
-            self.img[rowcs, colcs, :] = img_orig[rowcs, colcs, :]
-        '''
         self.imdata.set_data(self.img)
-        self.redrawline()
-    
-    def disconnect(self):
-        self.lasso.disconnect_events()
-        self.canvas.draw_idle()
-    
-    def redrawline(self):
+        
         if self.lasso.useblit:
             self.lasso.canvas.restore_region(self.lasso.background)
             self.lasso.ax.draw_artist(self.lasso.line)
@@ -269,30 +223,33 @@ class SelectFromCollection(object):
         else:
             self.lasso.canvas.draw_idle()
         plt.draw()
+        print_instructions()
+    
+    def disconnect(self):
+        self.lasso.disconnect_events()
+        self.canvas.draw_idle()
 
 
-def ravel_index(pos, shape):
-    res = 0
-    acc = 1
-    for pi, si in zip(reversed(pos), reversed(shape)):
-        res += pi * acc
-        acc *= si
-    return res
 
+def print_instructions():
+    print
+    print 'Draw a selection by holding down the left mouse button'
+    print 'Press the Fraction slider with the left mouse button to claim points inside the selection'
+    print 'Press a to select all points in the figure'
+    print 'Press 1 to claim all points in selection into class 1'
+    print 'Press 0 to claim all points in selection into class 0'
+    print 'Press l to lock all selected points to their current classes (e.g. they can not be claimed)'
+    print 'Press u to unlock all selected points after which they can be claimed again'
+    print 'Press c to perform a cyclic descent in the selection'
+    print
 
 
 selector = SelectFromCollection(plt.gca(), pcoll, mmc, img)
-'''def foo():
-            rows, cols = [], []
-            for i in range(selector.claims):
-                steepestdir = mmc.claim_a_point(1)
-                col, row = selector.collection[steepestdir]
-                #print steepestdir, row, col
-                rows.append(row)
-                cols.append(col)
-            selector.recolor()'''
 
 plt.draw()
+       
+selector.redrawall()
+print 'All points are selected'
 
 plt.show(block=True)
 
